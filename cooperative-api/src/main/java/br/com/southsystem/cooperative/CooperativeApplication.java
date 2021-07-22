caf83d7b33f36d54c746f376755bd510706d9444
Cleiton Oliveira <idd_caraujo@uolinc.com>
2021-07-22 10:44:34 -0300

    API: start project

---
+++ cooperative-api/src/main/java/br/com/southsystem/cooperative/CooperativeApplication.java
@@ -0,0 +1,13 @@
+package br.com.southsystem.cooperative;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CooperativeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CooperativeApplication.class, args);
+	}
+
+}
