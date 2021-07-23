404a9416009a77512b3fc453d227366f6f8263ff
Cleiton Oliveira <idd_caraujo@uolinc.com>
2021-07-22 22:54:04 -0300

    API: pageable DTO

---
+++ cooperative-api/src/main/java/br/com/southsystem/cooperative/dto/pageable/PageResponse.java
@@ -0,0 +1,42 @@
+package br.com.southsystem.cooperative.dto.pageable;
+
+
+import lombok.*;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+@Data
+public class PageResponse<T> implements Serializable {
+
+    private static final long serialVersionUID = -8859900022644419310L;
+
+    private List<T> content;
+    private int page;
+    private int size;
+    private int totalPages;
+    private int totalSize;
+    private long totalItems;
+    private Map<String, Object> filters;
+
+    public PageResponse(Page page){
+        buildResponse(page, page.getContent());
+    }
+
+    public PageResponse(Page page, List<T> c){
+        buildResponse(page, c);
+    }
+
+    private void buildResponse(Page page, List<T> c) {
+        setContent(c);
+        setPage(page.getNumber() + 1);
+        setSize(page.getSize());
+        setTotalPages(page.getTotalPages());
+        setTotalItems(page.getTotalElements());
+        setTotalSize(page.getNumberOfElements());
+    }
+}
---
+++ cooperative-api/src/main/java/br/com/southsystem/cooperative/dto/pageable/PageableRequest.java
@@ -0,0 +1,60 @@
+package br.com.southsystem.cooperative.dto.pageable;
+
+
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+
+public class PageableRequest {
+
+    private int page = 1;
+    private int size = 10;
+    private String sortBy = "";
+
+    private int maxPageSize = 20;
+
+
+    public PageableRequest(int page, int size, String sortField) throws IllegalArgumentException{
+        this.page = page;
+        this.size = size;
+        this.sortBy = sortField;
+        this.validate();
+    }
+
+
+    public PageableRequest(int page, int size, String sortField, int maxPageSize) throws IllegalArgumentException{
+        this.page = page;
+        this.size = size;
+        this.sortBy = sortField;
+        this.maxPageSize = maxPageSize;
+        this.validate();
+    }
+
+    public PageableRequest(int page, int size) throws IllegalArgumentException{
+        this.page = page;
+        this.size = size;
+        this.validate();
+    }
+
+
+    private void validate(){
+        if (page <= 0) {
+            throw new IllegalArgumentException("O numero da página deve ser maior que 0!");
+        }
+
+        if (size < 1 || size > maxPageSize) {
+            throw new IllegalArgumentException(String.format("o parâmtro size deve ser maior que 1 e menor que %d !", maxPageSize));
+        }
+
+        this.page = --page;
+        this.size = size;
+    }
+
+
+    public Pageable build(){
+        if(this.sortBy == null || this.sortBy.isEmpty()) {
+            return PageRequest.of( page,size, Sort.by(Sort.Direction.DESC, "createdAt"));
+        }
+        return PageRequest.of( page,size, Sort.by(Sort.Direction.DESC, this.sortBy));
+    };
+}
