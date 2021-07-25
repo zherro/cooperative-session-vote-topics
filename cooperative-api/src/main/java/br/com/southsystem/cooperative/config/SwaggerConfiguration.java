ef4335930c3b4c3a22f883a998fc50c8d36ca728
Cleiton Oliveira <idd_caraujo@uolinc.com>
2021-07-25 09:12:21 -0300

    API: swaguer api v2 config

--- cooperative-api/src/main/java/br/com/southsystem/cooperative/config/SwaggerConfiguration.java
+++ cooperative-api/src/main/java/br/com/southsystem/cooperative/config/SwaggerConfiguration.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
@@ -15,14 +16,29 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableSwagger2
 public class SwaggerConfiguration extends WebMvcConfigurationSupport {
 
-    ApiInfo info = new ApiInfoBuilder().title("Cooperative API").description("Cooperative Session API").build();
-
     @Bean
-    public Docket apiDocket(){
+    public Docket apiDocket() {
+        ApiInfoBuilder info = new ApiInfoBuilder().title("Cooperative API").description("Cooperative Session API v1");
+
         return new Docket(DocumentationType.SWAGGER_2)
-                .apiInfo(info)
+                .apiInfo(info.version("1.0").build())
+                .groupName("cooperative-api-v1.0")
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("br.com.southsystem.cooperative"))
+                .paths(PathSelectors.ant("/api/v1/**"))
+                .build();
+    }
+
+    @Bean
+    public Docket apiDocketV2() {
+        ApiInfoBuilder info = new ApiInfoBuilder().title("Cooperative API").description("Cooperative Session API v2");
+
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(info.version("2.0").build())
+                .groupName("cooperative-api-v2.0")
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("br.com.southsystem.cooperative"))
+                .paths(PathSelectors.ant("/api/v2/**"))
                 .build();
     }
 
